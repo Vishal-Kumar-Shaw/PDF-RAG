0e54f93602a3a67e696b92bc771ea52a03a0b227
vishal <vishal@vishals-MacBook-Air-2.local>
2025-08-04 09:48:07 +0530

    setup and initial functionality ui to backend

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import cors from 'cors';
+import multer from 'multer';
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, `${uniqueSuffix}-${file.originalname}`);
+  }
+})
+
+const app = express();
+app.use(cors());
+
+
+
+const upload = multer({ storage: storage })
+
+
+app.get('/', (req, res)=>{
+    console.log("All good!");
+    return res.json("All good");
+})
+
+app.post('/upload/pdf', upload.single('pdf'), (req, res)=>{
+    return res.json("File uploaded");
+})
+
+app.listen(8001, ()=>{
+    console.log('server started at port 8000');
+})
